e54277a4ae280ff1e7205512b5dd0637f8f6c0a0
higgs54 <maxwelllee54@gmail.com>
2017-02-17 17:50:10 -0500

    update

--- ImpliedVolatility.py
+++ ImpliedVolatility.py
@@ -1,11 +1,11 @@
-import math as e
+import numpy as np
 from scipy import stats
 from datetime import date
 import time
 
 
 class ImpliedVolatility():
-    def __init__(self, S, K, T, r, sigma, cStar, optionType, err = 1e-15):
+    def __init__(self, S, K, T, r, sigma, cStar, optionType, maxIter = 1e10, tolerance = 1e-10):
         self.S = S
         self.K = K
         self.T = T
@@ -13,34 +13,35 @@ class ImpliedVolatility():
         self.sigma = sigma
         self.cStar = cStar
         self.optionType = optionType
-        self.err = float(err)
+        self.maxIter = int(maxIter)
+        self.tolerance =tolerance
 
     def bsmValue(self, sigma = None):
         if sigma == None:
             sigma = self.sigma
 
-        d1 = (e.log(self.S / self.K) + (self.r + 0.5 * sigma ** 2) * self.T) / (sigma * e.sqrt(self.T))
-        d2 = d1 - sigma * e.sqrt(self.T)
+        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * sigma ** 2) * self.T) / (sigma * np.sqrt(self.T))
+        d2 = d1 - sigma * np.sqrt(self.T)
 
         if self.optionType in ['Call', 'call', 'CALL']:
 
-            return self.S * stats.norm.cdf(d1) - self.K * e.exp(-self.r * self.T) * stats.norm.cdf(d2)
+            return self.S * stats.norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * stats.norm.cdf(d2)
 
         elif self.optionType in ['Put', 'put', 'PUT']:
 
-            return self.K * e.exp(-self.r * self.T) * stats.norm.cdf(-d2) - self.S * stats.norm.cdf(-d1)
+            return self.K * np.exp(-self.r * self.T) * stats.norm.cdf(-d2) - self.S * stats.norm.cdf(-d1)
 
         else:
             raise TypeError('the option_type argument must be either "call" or "put"')
 
     def bsmVega(self):
-        d1 = (e.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * e.sqrt(self.T))
-        vega = self.S * stats.norm.pdf(d1) * e.sqrt(self.T)
+        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+        vega = self.S * stats.norm.pdf(d1) * np.sqrt(self.T)
         return vega
 
     def bsmVomma(self):
-        d1 = (e.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * e.sqrt(self.T))
-        d2 = d1 - self.sigma * e.sqrt(self.T)
+        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+        d2 = d1 - self.sigma * np.sqrt(self.T)
 
         return self.bsmVega() * d1 * d2 / self.sigma
 
@@ -58,37 +59,45 @@ class ImpliedVolatility():
         C = f2
 
         if B < 0:
-            x3 = x2 - 2 * C / (B - e.sqrt(B ** 2 - 4 * A * C))
+            x3 = x2 - 2 * C / (B - np.sqrt(B ** 2 - 4 * A * C))
         else:
-            x3 = x2 - 2 * C / (B + e.sqrt(B ** 2 - 4 * A * C))
+            x3 = x2 - 2 * C / (B + np.sqrt(B ** 2 - 4 * A * C))
 
         return x3
 
-    def bsmBisectionVol(self):
-        lower = self.err
-        upper = 100
+    def bsmBisectionVol(self, upper = 5):
+        lower = 1e-15
         middle = (lower + upper)/2
-        while e.fabs(self.f(middle)) > self.err:
+        for i in range(self.maxIter):
+            old_middle = (lower + upper) / 2
             if self.f(lower) * self.f(middle) < 0:
                 upper = middle
                 middle = (lower + upper) / 2
             else:
                 lower = middle
                 middle = (lower + upper) / 2
+
+            if np.fabs(old_middle - middle) < self.tolerance:
+                return middle
+
         return middle
 
     def bsmNewtonVol(self):
-        while self.f() > self.err:
-            self.sigma = self.sigma - self.f()/self.bsmVega()
+        for i in range(self.maxIter):
+            sigma = self.sigma
+            self.sigma = sigma - self.f()/self.bsmVega()
+            if np.fabs(sigma - self.sigma) < self.tolerance:
+                return self.sigma
+
         return self.sigma
 
-    def bsmMullerBisectionVol(self):
-        lower = self.err
-        upper = 100
+    def bsmMullerBisectionVol(self, upper = 5):
+        lower = 1e-15
         middle = (lower + upper) / 2
 
-        while e.fabs(self.f(middle)) > self.err:
+        for i in range(self.maxIter):
             muller = self.bsmMuller(lower, upper, middle)
+            old_middle = (lower + upper) / 2
             if self.f(lower) * self.f(middle) < 0:
                 upper = middle
             else:
@@ -99,52 +108,63 @@ class ImpliedVolatility():
             else:
                 middle = muller
 
+            if np.fabs(old_middle - middle) < self.tolerance:
+                return middle
+
         return middle
 
     def bsmHalley(self):
-        while self.f() > self.err:
-            self.sigma = self.sigma + (-self.bsmVega() + e.sqrt(self.bsmVega() ** 2 - 2 * self.f() * self.bsmVomma())) / self.bsmVomma()
+        newSigma = self.sigma
+        for i in range(self.maxIter):
+
+            newSigma = self.sigma + (-self.bsmVega() + np.sqrt(self.bsmVega() ** 2 - 2 * self.f() * self.bsmVomma())) / self.bsmVomma()
+            self.sigma = newSigma
+
+            if np.fabs(newSigma - self.sigma) < self.tolerance:
+                return self.sigma
+
         return self.sigma
 
 
+if __name__ == '__main__':
 
-today = date.today()
-expDay = date(2016, 12, 16)
-T = expDay - today
-sigma0 = 0.5
-r = 0.02 # 3-month T-bill rate
-currentStockPrice = 16.26
+    today = date(2016,11,15)
+    expDay = date(2016, 12, 16)
+    T = expDay - today
+    sigma0 = 0.5
+    r = 0.02 # 3-month T-bill rate
+    currentStockPrice = 16.26
 
 
-bacOptionList = [[currentStockPrice, 16.00, T.days / 365, r , sigma0, 0.77, 'call'],
+    bacOptionList = [[currentStockPrice, 16.00, T.days / 365, r , sigma0, 0.77, 'call'],
                  [currentStockPrice, 17.00, T.days / 365, r, sigma0, 0.31, 'call'],
                  [currentStockPrice, 16.00, T.days / 365, r, sigma0, 0.56, 'put'],
                  [currentStockPrice, 17.00, T.days / 365, r, sigma0, 1.12, 'put']]
 
 
-print('The underlying asset is Bank of America (BAC), current stock price is ${:.2f}, the expiration date is {:%Y-%m-%d}\n'.format(currentStockPrice, expDay))
-print('Now, let\'s begin:\n')
+    print('The underlying asset is Bank of America (BAC), current stock price is ${:.2f}, the expiration date is {:%Y-%m-%d}\n'.format(currentStockPrice, expDay))
+    print('Now, let\'s begin:\n')
 
-time_start = time.clock()
-for option in bacOptionList:
-    impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmBisectionVol()
-    print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.16%}\n'.format(option[6], option[1], option[5], impvol))
-print('The Bisection method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
+    time_start = time.clock()
+    for option in bacOptionList:
+        impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmBisectionVol()
+        print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.16%}\n'.format(option[6], option[1], option[5], impvol))
+    print('The Bisection method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
 
-time_start = time.clock()
-for option in bacOptionList:
-    impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmMullerBisectionVol()
-    print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.16%}\n'.format(option[6], option[1], option[5], impvol))
-print('The Muller-Bisection method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
+    time_start = time.clock()
+    for option in bacOptionList:
+        impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmMullerBisectionVol()
+        print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.16%}\n'.format(option[6], option[1], option[5], impvol))
+    print('The Muller-Bisection method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
 
-time_start = time.clock()
-for option in bacOptionList:
-    impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmNewtonVol()
-    print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.16%}\n'.format(option[6], option[1], option[5], impvol))
-print('The Newton method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
+    time_start = time.clock()
+    for option in bacOptionList:
+        impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmNewtonVol()
+        print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.16%}\n'.format(option[6], option[1], option[5], impvol))
+    print('The Newton method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
 
-time_start = time.clock()
-for option in bacOptionList:
-    impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmHalley()
-    print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.15%}\n'.format(option[6], option[1], option[5], impvol))
-print('The Halley method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
+    time_start = time.clock()
+    for option in bacOptionList:
+        impvol = ImpliedVolatility(option[0], option[1], option[2], option[3], option[4], option[5], option[6]).bsmHalley()
+        print('Here is a {0} option.\nThe strike price is ${1:.2f} and option price is ${2:.2f}.\nThe implied volatility is {3:.15%}\n'.format(option[6], option[1], option[5], impvol))
+    print('The Halley method takes {:.4f} seconds to run\n'.format(time.clock() - time_start))
---
+++ OptionDataWebGleaner2.py
@@ -0,0 +1,411 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import numpy as np
+import pandas as pd
+from datetime import datetime, date
+import os
+
+
+def main():
+    timeStart = datetime.now()
+    tickerList = pd.read_csv('NasdaqStockList.csv', header=None).iloc[:, 0].values.tolist()
+    #tickerList = np.random.choice(tickerList, 50, replace=False)
+    print(tickerList)
+    for ticker in tickerList:
+        data = OptionDataWebGleaner(ticker=ticker).saveFile()
+
+    print('The download is complete. Running time: {:.2f} mins'.format((datetime.now() - timeStart).seconds/60))
+
+class OptionDataWebGleaner():
+    '''
+        Let user to scrape option data from NASDAQ option chain:
+
+        Input:
+        ticker:     ticker for the underlying stock
+        dateindex:  expiration date
+            - 0(default): current month
+            - 1         : 1st nearby month
+            - 2         : 2nd nearby month
+            ....
+        expir:      expiration gap
+            - default       :   all
+            - stan          :   monthly
+            - week          :   weekly
+            - quart         :   quarterly
+            ...
+
+        money:  strick price's moneyness
+            - default       :   near the money
+            - in            :   in-the-money
+            - out           :   out-the-money
+            - all           :   all  the money
+
+        callput:    call or put options
+            - default       :   all
+            - call          :   call option
+            - put           :   put option
+            ...
+
+        excode:     option exchange
+            - default       :   all
+            - composite     :   composite quote
+            - cbo           :   Chicago Board Options Exchange
+            - aoe           :   American Options Exchange
+            - nyo           :   New York Options Exchange
+            - pho           :   Philadelphia Options Exchange
+            - moe           :   Montreal Options Exchange
+            - box           :   Boston Options Exchange
+            - ise           :   International Securities Exchange
+            - bto           :   Bats Exchange Options Market
+            - nso           :   NASDAQ Options
+            - c2o           :   C2(Chicago) Options Exchange
+            - bxo           :   Bats Exchange Options Market
+            - miax          :   MIAX
+        '''
+
+    def __init__(self, ticker=None, dateIndex=0, expir='', money='all', exCode=''):
+        self.ticker = ticker
+        self.dateIndex = dateIndex
+        self.expir = expir
+        self.money = money
+        self.exCode = exCode
+
+    # get the content of a certain page
+    def getUrl(self, page=1):
+
+        url = 'http://www.nasdaq.com/symbol/' + self.ticker + '/option-chain?dateindex=' + str(
+            self.dateIndex) + '&expir=' + self.expir + '&money=' + self.money + '&excode=' \
+              + self.exCode + '&page=' + str(page)
+
+        try:
+            r = requests.get(url)
+
+            nasdaqPage = BeautifulSoup(r.content, 'html.parser')
+
+            return nasdaqPage
+
+        except ConnectionError:
+            print('Connection error, try again!')
+
+    def getGreeksUrl(self, page=1):
+        url = 'http://www.nasdaq.com/symbol/' + self.ticker + '/option-chain/greeks?dateindex=' + str(
+            self.dateIndex) + '&page=' + str(page)
+        try:
+            r = requests.get(url)
+
+            greeksPage = BeautifulSoup(r.content, 'html.parser')
+
+            return greeksPage
+
+        except ConnectionError:
+
+            print('Connection error, try again!')
+
+
+    # get all pages' option information and put into DataFrame
+    def getOptions(self) -> object:
+        df = pd.DataFrame()
+        lastPage = self.getUrl().find('a', {'id': 'quotes_content_left_lb_LastPage'})
+        lastPageNo = re.findall(pattern='(?:page=)(\d+)', string=str(lastPage))
+        pageNo = ''.join(lastPageNo)
+
+        if pageNo == '':
+            pageNo = 1
+        else:
+            pageNo = int(pageNo)
+
+        for i in range(pageNo):
+
+            table = self.getUrl(i + 1).find_all('table')[5]  # the sixth table contains the option information
+            content = table.find_all('td')
+            lst = [text.text for text in content]
+
+            if not lst:
+                return df
+
+            try:
+                arr = np.array(lst).reshape((len(lst) // 16, 16))
+                dfTemp = pd.DataFrame(arr)
+                df = pd.concat([df, dfTemp])
+
+            except ValueError:
+                print('options error')
+
+
+        # make sure the index format in options and greeks are the same, for later dateframe merge
+        df.iloc[:, 8] = df.iloc[:, 8].astype(float)
+
+        tuples = list(zip(df.iloc[:, 0], df.iloc[:, 8]))
+
+        index = pd.MultiIndex.from_tuples(tuples, names=['ExpDate', 'StrikePrice'])
+
+        df.rename(columns={8: 'StrikePrice'}, inplace=True)
+
+        callOptions = df.iloc[:, 0:9]
+        putOptions = df.iloc[:, 7:17]
+
+        callOptions = callOptions.set_index(index)
+        putOptions = putOptions.set_index(index)
+
+        callheader = ['ExpDate', 'Last', 'Chg', 'Bid', 'Ask', 'Vol', 'Open Int', 'Ticker', 'StrikePrice']
+        putheader = ['Ticker', 'StrikePrice', 'ExpDate', 'Last', 'Chg', 'Bid', 'Ask', 'Vol', 'Open Int']
+        callOptions.columns = callheader
+        putOptions.columns = putheader
+
+        # reorder the columns
+        callOptions = callOptions[['Ticker', 'Last', 'Chg', 'Bid', 'Ask', 'Vol', 'Open Int']]
+        putOptions = putOptions[['Ticker', 'Last', 'Chg', 'Bid', 'Ask', 'Vol', 'Open Int']]
+
+        # in order to drop empty strike price rows, replace empty strings with NaN
+        callOptions['Last'].replace('', np.nan, inplace=True)
+        putOptions['Last'].replace('', np.nan, inplace=True)
+
+        # manage the datatypes
+        #callOptions.iloc[:, 5] = callOptions.iloc[:, 5].astype(int)
+        #putOptions.iloc[:, 5] = putOptions.iloc[:, 5].astype(int)
+
+        # make sure it returns the correct option data for the underlying asset
+        callOptions = callOptions[callOptions.Ticker == self.ticker.upper()]
+        putOptions = putOptions[putOptions.Ticker == self.ticker.upper()]
+
+        # get geeks
+        lastPage = self.getGreeksUrl().find('a', {'id': 'quotes_content_left_lb_LastPage'})
+        lastPageNo = re.findall(pattern='(?:page=)(\d+)', string=str(lastPage))
+        pageNo = ''.join(lastPageNo)
+
+        if pageNo == '':
+            pageNo = 1
+        else:
+            pageNo = int(pageNo)
+
+        df = pd.DataFrame()
+
+        for i in range(pageNo):
+            table = self.getGreeksUrl(i + 1).find_all('table')[5]
+            content = table.find_all('td')
+            lst = [text.text for text in content]
+
+            if not lst:
+                return df
+
+            try:
+
+                arr = np.array(lst).reshape((len(lst) // 16, 16))
+                dfTemp = pd.DataFrame(arr)
+                df = pd.concat([df, dfTemp])
+
+            except ValueError:
+                print('greeks error')
+
+        # make sure the index format in options and greeks are the same, for later dateframe merge
+        df.iloc[:, 8] = df.iloc[:, 8].astype(float)
+
+        tuples = list(zip(df.iloc[:, 0], df.iloc[:, 8]))
+
+        index = pd.MultiIndex.from_tuples(tuples, names=['ExpDate', 'StrikePrice'])
+
+        df.rename(columns={8: 'StrikePrice'}, inplace=True)
+
+        callGreeks = df.iloc[:, 0:9]
+        putGreeks = df.iloc[:, 7:17]
+
+        callGreeks = callGreeks.set_index(index)
+        putGreeks = putGreeks.set_index(index)
+
+        callheader = ['ExpDate', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV', 'Ticker', 'StrikePrice']
+        putheader = ['Ticker', 'StrikePrice', 'ExpDate', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']
+        callGreeks.columns = callheader
+        putGreeks.columns = putheader
+
+        # reorder and trim the columns
+        callGreeks = callGreeks[['Ticker', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']]
+        putGreeks = putGreeks[['Ticker', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']]
+
+        # manage the datatypes
+        try:
+            callGreeks.iloc[:, 1:] = callGreeks.iloc[:, 1:].astype(float)
+            putGreeks.iloc[:, 1:] = putGreeks.iloc[:, 1:].astype(float)
+        except ValueError:
+            pass
+
+        # in order to drop empty strike price rows, replace empty strings with NaN
+        callGreeks['IV'].replace('', np.nan, inplace=True)
+        putGreeks['IV'].replace('', np.nan, inplace=True)
+
+        # add cleaned greeks to the option
+        callOptions = pd.concat([callOptions, callGreeks[callGreeks.Ticker == self.ticker.upper()][['Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']]], axis=1)
+        putOptions = pd.concat([putOptions, putGreeks[putGreeks.Ticker == self.ticker.upper()][['Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']]], axis=1)
+
+        # add types to the dataframe
+        callOptions.insert(1, 'Type', 'Call')
+        putOptions.insert(1, 'Type', 'Put')
+
+        options = pd.concat([callOptions, putOptions])
+
+        # add crawling date to the dataframe
+        if options.empty:
+            print('No options crawled\n')
+        else:
+            options.loc[:, 'currentDate'] = date.today()
+
+        return options
+
+    # will return call options only
+    def getCall(self):
+        yield self.getOptions()[self.getOptions().Type == 'Call']
+
+    # will return put options only
+    def getPut(self):
+        yield self.getOptions()[self.getOptions().Type == 'Put']
+
+
+    def saveFile(self):
+
+        filename1 = 'DirtyData.csv'
+        filename2 = 'CleanData.csv'
+
+
+        df = pd.DataFrame()
+        dfTemp = pd.DataFrame()
+
+        print(self.ticker)
+        # find all expiration date
+        lastDate = self.getUrl().find('div', {'id': 'OptionsChain-dates'})
+        lastDateIndex = re.findall(pattern='(?:dateindex=)(\d+)', string=str(lastDate))
+
+        if not lastDateIndex:
+            expDate = 0
+        else:
+            expDate = int(lastDateIndex[-1])
+        try:
+
+            for date in range(expDate + 1):
+                self.dateIndex = date
+                dfTemp = self.getOptions()
+                if dfTemp.empty:
+                    break
+                else:
+                    df = pd.concat([df, dfTemp])
+        except KeyError:
+            pass
+        except IndexError:
+            pass
+
+
+        #control the csv format so that it has only one column name in first row
+        if os.path.isfile(filename1) == False:
+            mode = 'w'
+            header = True
+        else:
+            mode = 'a'
+            header = False
+
+        if not df.empty:
+
+            df.to_csv(filename1, mode=mode, header=header)
+            try:
+                df = df.loc[(df['IV'] > 0.00) & (df['IV'] < 2.00)]
+                df = df.dropna(subset=['Last'])
+            except:
+                pass
+
+            df.to_csv(filename2, mode=mode, header=header)
+
+'''
+    def getAllClean(self, data, N=2000):
+        options = {}
+        filename = 'CleanData.csv'
+        tickerList = np.random.choice(list(data.keys()), min(N, len(list(data.keys()),)), replace=False)
+
+        for ticker in tickerList:
+            try:
+                df = data[ticker]
+                # clean the outliers and missing values
+                df = df.loc[(df['IV'] > 0.00) & (df['IV'] < 2.00)]
+                #df = df.loc[df['Vol'] > 0]
+                df = df.dropna(subset=['Last'])
+
+                if os.path.isfile(filename):
+                    header = False
+                else:
+                    header = True
+
+                with open(filename, 'a') as f:
+                        df.to_csv(f, header=header)
+
+            except:
+                continue
+
+            options[ticker] = df
+        return options
+
+
+        def getGreeks(self):
+
+            lastPage = self.getGreeksUrl().find('a', {'id': 'quotes_content_left_lb_LastPage'})
+            lastPageNo = re.findall(pattern='(?:page=)(\d+)', string=str(lastPage))
+            pageNo = ''.join(lastPageNo)
+
+            if pageNo == '':
+                pageNo = 1
+            else:
+                pageNo = int(pageNo)
+
+            df = pd.DataFrame()
+
+            for i in range(pageNo):
+                table = self.getGreeksUrl(i + 1).find_all('table')[5]
+                content = table.find_all('td')
+                lst = [text.text for text in content]
+
+                if not lst:
+                    return df
+
+                try:
+
+                    arr = np.array(lst).reshape((len(lst) // 16, 16))
+                    dfTemp = pd.DataFrame(arr)
+                    df = pd.concat([df, dfTemp])
+
+                except ValueError:
+                    print('greeks error')
+
+            # make sure the index format in options and greeks are the same, for later dateframe merge
+            df.iloc[:, 8] = df.iloc[:, 8].astype(float)
+
+            tuples = list(zip(df.iloc[:, 0], df.iloc[:, 8]))
+
+            index = pd.MultiIndex.from_tuples(tuples, names=['ExpDate', 'StrikePrice'])
+
+            df.rename(columns={8: 'StrikePrice'}, inplace=True)
+
+            callGreeks = df.iloc[:, 0:9]
+            putGreeks = df.iloc[:, 7:17]
+
+            callGreeks = callGreeks.set_index(index)
+            putGreeks = putGreeks.set_index(index)
+
+            callheader = ['ExpDate', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV', 'Ticker', 'StrikePrice']
+            putheader = ['Ticker', 'StrikePrice', 'ExpDate', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']
+            callGreeks.columns = callheader
+            putGreeks.columns = putheader
+
+            # reorder and trim the columns
+            callGreeks = callGreeks[['Ticker', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']]
+            putGreeks = putGreeks[['Ticker', 'Delta', 'Gamma', 'Rho', 'Theta', 'Vega', 'IV']]
+
+            # manage the datatypes
+            callGreeks.iloc[:, 1:] = callGreeks.iloc[:, 1:].astype(float)
+            putGreeks.iloc[:, 1:] = putGreeks.iloc[:, 1:].astype(float)
+
+            # in order to drop empty strike price rows, replace empty strings with NaN
+            callGreeks['IV'].replace('', np.nan, inplace=True)
+            putGreeks['IV'].replace('', np.nan, inplace=True)
+            # make sure it returns the correct option data for the underlying asset
+
+            return callGreeks[callGreeks.Ticker == self.ticker.upper()], putGreeks[
+                putGreeks.Ticker == self.ticker.upper()]'''
+
+if __name__ == '__main__':
+    main()
